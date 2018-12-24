6a4818ffee2898e8a7f7612cb0cd6462f3650ac7
Brittani <brittani1128@gmail.com>
2018-12-24 13:32:51 -0600

    working app unit tests

--- client/src/App/App.test.js
+++ client/src/App/App.test.js
@@ -3,11 +3,6 @@ import { shallow } from "enzyme";
 import sinon from "sinon";
 import App from "./App";
 
-// test instance methods
-// shallow.instance
-// look up way to mock out context
-//
-
 describe("App component", () => {
   it("should render", () => {
     const wrapper = shallow(<App />);
@@ -15,30 +10,48 @@ describe("App component", () => {
   });
 
   describe("handleSelect", () => {
-    const mockFunction = jest.fn();
-    it("should set the state of searchQuery to event value", () => {});
-    // test state change
-    const instance = shallow(<App handleSubmit={mockFunction} />).instance();
-    instance.setState = sinon.spy();
-    instance.handleSelect();
-    expect(instance.setState.calledWith({}));
-  });
-
-  describe("handleSubmit", () => {
-    // test selectquery function called
-    const mockFunction = jest.fn();
-    it("should call selectQuery when clicked", () => {
-      const spy = sinon.spy();
+    it("should set the state of searchQuery to event.target.value", () => {
+      const eventMock = { target: { value: "test" } };
+      const instance = shallow(<App />).instance();
+      instance.setState = sinon.spy();
+      instance.handleSelect(eventMock);
+      expect(instance.setState.calledWith({ searchQuery: "test" })).toEqual(
+        true
+      );
     });
   });
 
-  //   describe("search input", () => {
-  //     it("should respond to change event and change state of searchQuery", () => {
-  //       const wrapper = shallow(<App />);
-  //       wrapper
-  //         .find("#input")
-  //         .simulate("change", { target: { name: "input", value: "yoda" } });
-  //       expect(wrapper.state("resultSearchQuery")).toEqual("yoda");
-  //     });
-  //   });
+  describe("handleDetails", () => {
+    it("should set the state of resultIndex to dataset index", () => {
+      const eventMock = { target: { dataset: { index: 1 } } };
+      const instance = shallow(<App />).instance();
+      instance.setState = sinon.spy();
+      instance.handleDetails(eventMock);
+      expect(instance.setState.calledWith({ resultIndex: 1 })).toEqual(true);
+    });
+  });
+
+  describe("handleInputChange", () => {
+    it("should set the state of resultSearchQuery to event.target.value", () => {
+      const eventMock = { target: { value: "yoda" } };
+      const instance = shallow(<App />).instance();
+      instance.setState = sinon.spy();
+      instance.handleInputChange(eventMock);
+      expect(
+        instance.setState.calledWith({ resultSearchQuery: "yoda" })
+      ).toEqual(true);
+    });
+  });
+
+  describe("handleSubmit", () => {
+    const eventMock = {
+      preventDefault: () => {}
+    };
+    it("should call selectQuery", () => {
+      const instance = shallow(<App />).instance();
+      const spy = sinon.spy(instance, "selectQuery");
+      instance.handleSubmit(eventMock);
+      expect(spy.called).toEqual(true);
+    });
+  });
 });
